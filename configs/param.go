96b4a3e2f5869e23de5c2a9c03344089997fe80d
vipxkw <47981717+vipxkw@users.noreply.github.com>
2025-10-19 23:21:23 +0800

    Update param.go
    增加docker环境变量配置

--- configs/param.go
+++ configs/param.go
@@ -1,7 +1,9 @@
+// filePath: ChinaTelecomMonitor/configs/param.go
 package configs
 
 import (
 	"China_Telecom_Monitor/models"
+	"os"
 	"go.uber.org/zap"
 )
 
@@ -13,16 +15,27 @@ var TimeOut int64
 var IntervalsTime int
 
 var DataPath string
-
 var LogLevel string
 var LogEncoding string
-
 var Dev bool
-
 var PrintVersion bool
-
 var ClientVersion string
 
 var Summary models.Summary
-
 var Logger *zap.SugaredLogger
+
+// 从环境变量加载配置
+func LoadFromEnv() {
+	if env := os.Getenv("TELECOM_USERNAME"); env != "" {
+		Username = env
+	}
+	if env := os.Getenv("TELECOM_PASSWORD"); env != "" {
+		Password = env
+	}
+	if env := os.Getenv("TELECOM_PROT"); env != "" {
+		Prot = env
+	}
+	if env := os.Getenv("TELECOM_CLIENT_VERSION"); env != "" {
+		ClientVersion = env
+	}
+}
